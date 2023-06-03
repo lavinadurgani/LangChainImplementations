17356b5ee1f7d6775dc41d2c934630bc52e4fcbc
lavinadurgani <33307795+lavinadurgani@users.noreply.github.com>
2023-06-03 08:41:40 -0700

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.title('🎈Ask Langchain and ChatGPT')
+
+st.write('Hello world!')
+
+openai_api_key = st.sidebar.text_input('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')
+
+def generate_response(input_text):
+  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+  st.info(llm(input_text))
+
+with st.form('my_form'):
+  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+  submitted = st.form_submit_button('Submit')
+  if not openai_api_key.startswith('sk-'):
+    st.warning('Please enter your OpenAI API key!', icon='⚠')
+  if submitted and openai_api_key.startswith('sk-'):
+    generate_response(text)
