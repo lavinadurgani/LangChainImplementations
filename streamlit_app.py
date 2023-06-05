dd9474b54d3462379c12901aa3ba8b627a0550df
lavinadurgani <33307795+lavinadurgani@users.noreply.github.com>
2023-06-05 09:59:03 -0700

    Update and rename streamlit_app.pyai to streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.write('Hello world!')
 openai_api_key = st.sidebar.text_input('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')
 
 def generate_response(input_text):
-  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+  llm = openai(temperature=0.7, openai_api_key=openai_api_key)
   st.info(llm(input_text))
 
 with st.form('my_form'):
